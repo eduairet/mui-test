8a356473bc038aaec9be531de4d582e9b73a65f9
eduairet <hola@eduairet.com>
2023-06-05 19:06:22 -0600

    Build test 001

--- src/components/TouchCard.tsx
+++ src/components/TouchCard.tsx
@@ -1,14 +1,21 @@
-import { Paper, Typography, Divider } from '@mui/material';
+import { Paper, Rating, Typography, Box, Divider } from '@mui/material';
+import { ElectricBolt } from '@mui/icons-material';
 import { ApiData } from '../store/api-context';
 
 export default function TouchCard({ name, incantation, effect, type }: ApiData) {
     return (
         <Paper elevation={8} sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '1rem 1rem 1.5rem 1rem' }}>
             <Typography variant="h5">{name}</Typography>
-            <Typography variant="h6">{incantation}</Typography>
+            <Typography variant="h6" sx={{ fontSize: '0.75rem', fontStyle: 'italic' }}>{incantation}</Typography>
+            <Divider sx={{ margin: '1rem 0' }} />
+            <Box>
+                {Array.from({ length: 1 + Math.round(Math.random() * 5) }).map((_, i) => <ElectricBolt key={`star-${i}`} color='primary' />)}
+            </Box>
             <Divider sx={{ margin: '1rem 0' }} />
             <Typography variant="subtitle1">{type}</Typography>
             <Typography variant="body1">{effect}</Typography>
+            <Divider sx={{ margin: '1rem 0' }} />
+            <Rating name="score" value={Math.random() * 5} readOnly precision={0.5} />
         </Paper >
     );
 }
